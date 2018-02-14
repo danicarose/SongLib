948e24f32c32dc590619dfb10dd4ea24a9dfb5f1
danicarose <danica.calusin@gmail.com>
2018-02-14 17:38:28 -0500

    reverting last commit

--- src/view/SongLibController.java
+++ src/view/SongLibController.java
@@ -18,8 +18,6 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ListView;
@@ -42,7 +40,6 @@ public class SongLibController {
 	//@FXML TextArea details;
 	
 	private ObservableList<String> obsList;
-	private File songLib=new File("songLib.json");; //file used to store songs 
 	JSONObject root = new JSONObject();
 	JSONArray songs = new JSONArray();
 
@@ -52,9 +49,8 @@ public class SongLibController {
 		root.put("songs", songs);
 		
 		obsList = FXCollections.observableArrayList();
-		songDisplay();
+		
 		listView.setItems(obsList);
-		listView.getSelectionModel().selectFirst();
 		//ensures first item in list is automatically selected at start
 		//need to write code for the listener 
 	}
@@ -65,7 +61,7 @@ public class SongLibController {
 	 * song objects are then parsed to the obsList 
 	 */
 	
-//Adds a song to the song list display	
+	//Adds a song to the song list display	
 private void songDisplay() {
 	JSONParser parser = new JSONParser();
 		
@@ -88,9 +84,7 @@ private void songDisplay() {
 
 //Writes song to json file
 	private void songFileHandler(){
-
 		songLib = new File("songLib.json");
-
 		try(FileWriter writer = new FileWriter(songLib)){
 			writer.write(root.toString());
 			writer.flush();
@@ -105,41 +99,7 @@ private void songDisplay() {
 	
 	//adds song to json file and makes call
 	private void onClick_add(ActionEvent e){
-		String songName="";
-		String artistName="";
-		String albumName="";
-		String songYearTxt="";
-		int songYear; 
-		JSONObject songObject = new JSONObject();
-		
-		//Checks if required song or artist fields are empty
-		if ((txt_name.getText().toString() == null ||txt_artist.getText().toString()==null) ||
-				(txt_name.getText().trim().isEmpty() || txt_artist.getText().trim().isEmpty())) {
-			Alert alert = new Alert(AlertType.ERROR);
-			alert.setTitle("ERROR");
-			alert.setHeaderText(null);
-			alert.setContentText("You must enter a song name and artist name");
-			alert.showAndWait();
-		}else {
-			songName=txt_name.getText().toString();
-			artistName=txt_artist.getText().toString();
-			songObject.put("song name", songName);
-			songObject.put("artist name", artistName);
-		}
-		
-		//Checks if album field is empty
-		if(!(txt_album.getText().toString().trim().isEmpty() || txt_album.getText().toString()==null)) {
-			albumName=txt_album.getText().toString();
-			songObject.put("album name", albumName);
-		}
-		
-		//Checks if song field is empty
-		if(!(txt_album.getText().toString().trim().isEmpty() || txt_album.getText().toString()==null)) {
-			songYearTxt=txt_year.getText().toString();
-			songYear=Integer.parseInt(songYearTxt);
-			songObject.put("year", songYear);
-		}
-
+	
 		String songName=txt_name.getText().toString();
 		String artistName=txt_artist.getText().toString();
 		String albumName=txt_album.getText().toString();
@@ -174,44 +134,27 @@ private void songDisplay() {
 		{
 			JSONObject p = (JSONObject) parser.parse(new FileReader("songLib.json"));
 			JSONArray songDisp =(JSONArray) p.get("songs");
-	
+			
+			/*for(int i=0; i< songDisp.size();i++) {
+				JSONObject songDisplay= (JSONObject) songDisp.get(i);
+				String sName = (String) songDisplay.get("song name");
+				String arName =(String) songDisplay.get("artist name");
+				obsList.add(sName + " by " + arName);
+			}*/
 			
 			JSONObject songDetailObj = (JSONObject) songDisp.get(index);
 			String sName = (String) songDetailObj.get("song name");
 			String arName = (String) songDetailObj.get("artist name");
 			String year = (String) songDetailObj.get("year");
 			String album = (String) songDetailObj.get("album");
-			/*
-			details.setText("Song title: " + sName);
-			details.setText("Artist: " + arName);
-			details.setText("Album: " + album);
-			details.setText("Year: " + year);*/
 		}
 		catch(FileNotFoundException e) {e.printStackTrace();}
 		catch(IOException e) {e.printStackTrace();}
 		catch(ParseException e) {e.printStackTrace();}
 	}
-		
-	//If duplicate song does not add
-	boolean songDup = duplicate(songObject);
-	if(songDup) {
-		songs.add(songObject);
-		obsList.add(songName + " by " + artistName);
-		songFileHandler();
-		listView.getSelectionModel().select(songName+ " by " +artistName);
-	}else {
-		Alert alert = new Alert(AlertType.ERROR);
-		alert.setTitle("ERROR");
-		alert.setHeaderText(null);
-		alert.setContentText("Sorry, that song is already in your library");
-		alert.showAndWait();
-	}
-	
-
-	}
 	
 	@FXML
-	private void onClick_delete(ActionEvent e){
+	private void onClick_delete(){
 		
 	}
 	
@@ -219,71 +162,16 @@ private void songDisplay() {
 	private void onClick_edit(){
 		//get index of selected song item
 		int index = listView.getSelectionModel().getSelectedIndex();
-
-	private void onClick_edit(ActionEvent e){
+		
+		
+		
 
 		//clear textboxes once song has been edited
 		txt_name.clear();
 		txt_artist.clear();
 		txt_album.clear();
 		txt_year.clear();
-=======
-	private void onClick_edit(ActionEvent e){
+		
 		
 	}
-	
-	private boolean duplicate(JSONObject songObject) {
-		JSONParser parser = new JSONParser();
-		try 
-		{
-			JSONObject p = (JSONObject) parser.parse(new FileReader("songLib.json"));
-			JSONArray duplicateCheck =(JSONArray) p.get("songs");
-			
-			for(int i=0; i< duplicateCheck.size();i++) {
-				JSONObject duplicate= (JSONObject) duplicateCheck.get(i);
-				String sName = (String) duplicate.get("song name");
-				String arName =(String) duplicate.get("artist name");
-				
-				String sName2= (String) songObject.get("song name");
-				String arName2= (String) songObject.get("artist name");
-				
-				if(sName.equalsIgnoreCase(sName2)&&arName.equalsIgnoreCase(arName2)) {
-					return false;
-			}
-		}
-		}
-		catch(FileNotFoundException e) {e.printStackTrace();}
-		catch(IOException e) {e.printStackTrace();}
-		catch(ParseException e) {e.printStackTrace();}
-		return true;
-	}
-	}
-
-	
-	private boolean duplicate(JSONObject songObject) {
-		JSONParser parser = new JSONParser();
-		try 
-		{
-			JSONObject p = (JSONObject) parser.parse(new FileReader("songLib.json"));
-			JSONArray duplicateCheck =(JSONArray) p.get("songs");
-			
-			for(int i=0; i< duplicateCheck.size();i++) {
-				JSONObject duplicate= (JSONObject) duplicateCheck.get(i);
-				String sName = (String) duplicate.get("song name");
-				String arName =(String) duplicate.get("artist name");
-				
-				String sName2= (String) songObject.get("song name");
-				String arName2= (String) songObject.get("artist name");
-				
-				if(sName.equalsIgnoreCase(sName2)&&arName.equalsIgnoreCase(arName2)) {
-					return false;
-			}
-		}
-		}
-		catch(FileNotFoundException e) {e.printStackTrace();}
-		catch(IOException e) {e.printStackTrace();}
-		catch(ParseException e) {e.printStackTrace();}
-		return true;
-	}
-	}
-
+}
